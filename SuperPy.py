74d4c93396e42b38fcebf45d8b384d3415b7ce3a
Mariskaatje <mariskaatje74@gmail.com>
2024-12-16 20:42:29 +0100

    Update SuperPy.py

--- SuperPy.py
+++ SuperPy.py
@@ -199,7 +199,7 @@ print(f'Over {number} dagen is het {date_after_number_days}')
 
 # products.csv
 
-# Interactieve invoer van productgegevens
+# Invoer van productgegevens
 products = []
 while True:
     id = input('Voer de id in ')
@@ -224,3 +224,96 @@ with open('products', "w", newline="") as csvfile:
         writer.writerow([product['id'], product['product_name'], product['bought_date'], product['bought_price'], product['expiration_date'], product['sell_date'], product['sell_price']])
 
 print(f"Productgegevens zijn opgeslagen in 'products' {products}")
+
+# Imports
+import argparse
+import csv
+import datetime
+
+# Do not change these lines.
+__winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
+__human_name__ = "superpy"
+
+# Your code below this line.
+def main():
+    pass
+
+# Vaststellen welke dag het is.
+
+today = datetime.date.today()
+print(f'Datum: {today}')
+
+# Van datetime naar string  
+
+datetime_to_string = today.strftime('%Y-%m-%d')
+
+print(f'Het is vandaag {datetime_to_string}')
+
+# Van string naar datetime
+
+# string_to_datetime = datetime.datetime.strptime(today_string, '%Y_%m_%d')
+# print(string_to_datetime)
+
+from datetime import timedelta
+today_date = datetime.date.today()
+
+number = int(input('Hoeveel dagen wilt u de datum vooruit zetten?' ))
+timedelta = datetime.timedelta(days=number)
+date_after_number_days = today_date + timedelta
+print(f'Over {number} dagen is het {date_after_number_days}')
+
+# (python super.py --advance-time 2)
+
+# products.csv
+
+# Invoer van productgegevens
+products = []
+while True:
+    id = input('Voer de id in ')
+    product_name = input('Voer de product_name in ')
+    bought_date = input('Voer de bought_date (YYYY-MM-DD) in ')
+    bought_price = float(input('Voer de bought_price in '))
+    expiration_date = input('Voer de expiration_date in (YYYY-MM-DD)')
+    sell_date = input('Voer de sell_date (YYYY-MM-DD) in ')
+    sell_price = float(input('Voer de sell_price in '))
+    
+    products.append({'id': id, 'product_name': product_name, 'bought_date': bought_date, 'bought_price': bought_price, 'expiration_date': expiration_date, 'sell_date': sell_date, 'sell_price': sell_price})
+    
+    doorgaan = input("Wil je nog een product toevoegen? (ja/nee): ").lower()
+    if doorgaan != "ja":
+        break
+
+# CSV-file schrijven
+with open('products', "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['id', 'product_name', 'bought_date', 'bought_price', 'expiration_date', 'sell_date', 'sell_price'])
+    for product in products:
+        writer.writerow([product['id'], product['product_name'], product['bought_date'], product['bought_price'], product['expiration_date'], product['sell_date'], product['sell_price']])
+
+print(f"Productgegevens zijn opgeslagen in 'products' {products}")
+
+# Totaal aantal gekochte en verkochte producten op bepaalde datum vaststellen
+
+datum = import('Voor welke datum (YYYY-MM-DD) wilt u het totale aantal gekochte en verkochte producten weten? ')
+
+def totaal_producten_met_bought_date(csv_file, datum):
+    totaal = 0
+    with open(csv_bestand, 'r') as file:
+        reader = csv.DictReader(file)
+        for rij in reader:
+            if rij['bought_date'] == datum:
+                totaal += 1
+    return totaal
+
+def totaal_producten_met_sell_date(csv_file, datum):
+    totaal1 = 0
+    with open(csv_bestand, 'r') as file:
+        reader = csv.DictReader(file)
+        for rij in reader:
+            if rij['sell_date'] == datum:
+                totaal1 += 1
+    return totaal1
+
+print(f"Totaal aantal gekochte producten op {datum}: {totaal} en aantal verkochte producten op {datum}: {totaa1l}")
+
+# Opbrengst en winst over bepaalde periode vaststellen
